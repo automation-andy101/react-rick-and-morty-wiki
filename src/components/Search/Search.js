e3fb6188cd7fc7a5ec29a4678f5a04d4f8ca5a76
Andy Short <automation.andy101@gmail.com>
2023-03-13 14:40:56 +0000

    first commit

---
+++ src/components/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Search.module.scss';
+
+const search = () => {
+  return (
+    <form className='d-flex justify-content-center gap-4 mb-5'>
+        <input placeholder='Search for characters' type='text' className={styles.input} />
+        <button className='btn btn-primary fs-5'>Search</button>
+    </form>
+  )
+}
+
+export default search;
